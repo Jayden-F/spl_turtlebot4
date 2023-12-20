94c41b765b4a4187f0107b923238d77fbbb0355e
jayden <jaydenjfilippi@gmail.com>
2023-12-20 14:01:45 +1100

    I hope this works

--- src/spl_packages/commander_server/commander_server/turtlebot4_commander.py
+++ src/spl_packages/commander_server/commander_server/turtlebot4_commander.py
@@ -1,57 +1,85 @@
 #!/usr/bin/env python3
+
 import json
 import os
+import argparse
 from math import cos, radians, sin
 from typing import Tuple
 
 import rclpy
 import requests
 from action_msgs.msg import GoalStatus
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 from nav2_msgs.action import NavigateToPose
 from nav2_msgs.action._navigate_to_pose import (
-    NavigateToPose_FeedbackMessage, NavigateToPose_GetResult_Response,
-    NavigateToPose_SendGoal_Response)
+    NavigateToPose_FeedbackMessage,
+    NavigateToPose_GetResult_Response,
+    NavigateToPose_SendGoal_Response,
+)
 from nav2_simple_commander.robot_navigator import BasicNavigator
 from rclpy.duration import Duration
+from rclpy.node import Node
+from rclpy.qos import (
+    QoSDurabilityPolicy,
+    QoSHistoryPolicy,
+    QoSProfile,
+    QoSReliabilityPolicy,
+)
 
 X = Y = Theta = float
 Position = Tuple[X, Y, Theta]
 
 
-class Turtlebot4_Commander(BasicNavigator):
+class Over_Here(Node):
+    def __init__(self) -> None:
+        amcl_pose_qos = QoSProfile(
+            durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
+            reliability=QoSReliabilityPolicy.RELIABLE,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=1,
+        )
+        self.localization_pose_sub = self.create_subscription(
+            PoseWithCovarianceStamped,
+            "amcl_pose",
+            self._amclPoseCallback,
+            amcl_pose_qos,
+        )
 
-    def __init__(self,
-                 hostName: str = "192.168.0.204",
-                 serverPort: int = 8080
-                ):
-    
+        def _amclPoseCallback(self, msg):
+            self.debug("Received amcl pose")
+            self.initial_pose_received = True
+            return
+
+
+class Turtlebot4_Commander(BasicNavigator):
+    def __init__(
+        self,
+        turtlebot4_id: int = 0,
+        hostName: str = "192.168.0.204",
+        serverPort: int = 8080,
+    ):
         super().__init__("turtlebot4_commander")
 
         # self.timer = self.create_timer(0.5, self.commander)
 
-        self.turtlebot4_id: int = os.uname()[1]
+        self.turtlebot4_id: int = turtlebot4_id
         self.hostName: str = hostName
         self.serverPort: int = serverPort
         self.isExecuting: bool = False
         self.pose: PoseStamped = None
         self.timestep: int = 0
-        print(f"Starting Commander Server:\nRobot ID: {self.turtlebot4_id}\nWebserver: {hostName}:{serverPort}")
+        print(
+            f"Starting Commander Server:\nRobot ID: {self.turtlebot4_id}\nWebserver: {hostName}:{serverPort}"
+        )
         self.run()
 
-
-
     def run(self):
-    
-
         data = self.get_request()
         position = data.get("position")
         self.timestep = data.get("timestep")
 
         pose = self.getPoseStamped(position)
         self.send_goal(pose)
-
-        
 
     def get_request(self):
         """Get the next position to navigate to."""
@@ -71,11 +99,17 @@ class Turtlebot4_Commander(BasicNavigator):
         """Post the current position to the server."""
         url = f"http://{self.hostName}:{self.serverPort}"
 
-        data = {"agent_id": self.turtlebot4_id,
-                "position": [position.pose.position.x, position.pose.position.y, position.pose.orientation.z],
-                "status": status,
-                "timestep": self.timestep}
-        
+        data = {
+            "agent_id": self.turtlebot4_id,
+            "position": [
+                position.pose.position.x,
+                position.pose.position.y,
+                position.pose.orientation.z,
+            ],
+            "status": status,
+            "timestep": self.timestep,
+        }
+
         json_data = json.dumps(data)
         r = None
         while r is None:
@@ -83,7 +117,6 @@ class Turtlebot4_Commander(BasicNavigator):
                 r = requests.post(url, data=json_data)
             except requests.ConnectionError:
                 r = None
-
 
     def getPoseStamped(self, position: Position) -> PoseStamped:
         """
@@ -95,7 +128,7 @@ class Turtlebot4_Commander(BasicNavigator):
         """
         pose = PoseStamped()
 
-        pose.header.frame_id = 'map'
+        pose.header.frame_id = "map"
         pose.header.stamp = self.get_clock().now().to_msg()
 
         pose.pose.position.x = position[0]
@@ -106,53 +139,61 @@ class Turtlebot4_Commander(BasicNavigator):
         pose.pose.orientation.w = cos(radians(position[2]) / 2)
 
         return pose
-        
+
     def send_goal(self, pose: Position):
         goal_msg = NavigateToPose.Goal()
         goal_msg.pose = pose
 
-        self.info('Navigating to goal: ' + str(pose.pose.position.x) + ' ' +
-            str(pose.pose.position.y) + '...')
-        
+        self.info(
+            "Navigating to goal: "
+            + str(pose.pose.position.x)
+            + " "
+            + str(pose.pose.position.y)
+            + "..."
+        )
+
         self.nav_to_pose_client.wait_for_server()
 
-        self._send_goal_future = self.nav_to_pose_client.send_goal_async(goal_msg, self.feedback_callback)
+        self._send_goal_future = self.nav_to_pose_client.send_goal_async(
+            goal_msg, self.feedback_callback
+        )
 
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     def goal_response_callback(self, future):
-
         goal_handle: NavigateToPose_SendGoal_Response = future.result()
         if not goal_handle.accepted:
-            self.get_logger().info('Goal rejected :(')
+            self.get_logger().info("Goal rejected :(")
             return
 
-        self.get_logger().info('Goal accepted :)')
-        
+        self.get_logger().info("Goal accepted :)")
 
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
 
-
     def feedback_callback(self, msg: NavigateToPose_FeedbackMessage):
-
-        self.debug('Received action feedback message')
+        self.debug("Received action feedback message")
         feedback = msg.feedback
         self.pose = feedback.current_pose
         position = feedback.current_pose.pose._position
         orientation = feedback.current_pose.pose.orientation
         eta = Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9
         eda = feedback.distance_remaining
-        
-        self.info("Pose: " + str([position.x, position.y]) + " eta: " + str(eta) + " eda: " + str(eda))
+
+        self.info(
+            "Pose: "
+            + str([position.x, position.y])
+            + " eta: "
+            + str(eta)
+            + " eda: "
+            + str(eda)
+        )
         self.post_request(self.pose, "Executing")
 
-
     def get_result_callback(self, future):
-
         reponse: NavigateToPose_GetResult_Response = future.result()
         status = reponse.status
-        
+
         status_string = None
         match status:
             case GoalStatus.STATUS_SUCCEEDED:
@@ -166,16 +207,16 @@ class Turtlebot4_Commander(BasicNavigator):
 
         self.post_request(self.pose, status_string)
         self.info("Goal Status: " + status_string)
-        
+
         self.run()
 
 
-
-def main(args=None):
-
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--id", type=int, default=0, dest="turtlebot4_id")
+    args = parser.parse_args()
     rclpy.init(args=args)
-    commander = Turtlebot4_Commander()
-
+    commander = Turtlebot4_Commander(args.turtlebot4_id)
     try:
         rclpy.spin(commander)
     except KeyboardInterrupt:
@@ -185,5 +226,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
