4d744a400220eab106200ee24482b49b3b9ab02e
jayden <jaydenjfilippi@gmail.com>
2024-01-12 16:56:38 +1100

    feat: cpp turtlebot4 commander server.

--- src/spl_packages/commander_server/commander_server/turtlebot4_commander.cpp
+++ src/spl_packages/commander_server/commander_server/turtlebot4_commander.cpp
@@ -1,54 +1,61 @@
 #include "commander_server/turtlebot4_commander.hpp"
 
 commander_server::turtlebot4_commander::turtlebot4_commander(
-    int robot_id, const std::string &ip, const std::string &port,
+    const uint32_t id, const std::string &ip, const uint16_t port,
     const rclcpp::NodeOptions &options)
-    : Node("Turtlebot4_Commander", options), ip_(ip), port_(port),
-      id_(robot_id) {
-  RCLCPP_INFO(this->get_logger(),
-              "Starting commander_server::turtlebot4_commander");
+    : Node("Turtlebot4_Commander", options), ip_(ip), port_(port), id_(id),
+      socket_(io_service_) {
+  RCLCPP_INFO(
+      this->get_logger(),
+      "Starting turtlebot4_commander\n Agent ID: %d\n IP: %s\n Port: %d", id_,
+      ip_, port_);
 
-  this->navigate_to_pose_client_ptr_ =
+  navigate_to_pose_client_ptr_ =
       rclcpp_action::create_client<NavigateToPose>(this, "/navigate_to_pose");
+  pose_subscriber_ptr_ = create_subscription<PoseWithCovariance>(
+      "/amcl_pose", 10,
+      std::bind(&commander_server::turtlebot4_commander::pose_topic_callback,
+                this, std::placeholders::_1));
 
   // Connect to the central controller
-  boost::asio::io_service io_service;
-  sock_(io_service);
   try {
-    sock_.connect(
-        ip::tcp::endpoint(ip::address::from_string(ip_), std::stoi(port_)));
+    socket_.connect(boost::asio::ip::tcp::endpoint(
+        boost::asio::ip::address::from_string(ip_), port_));
   } catch (boost::system::system_error &e) {
     RCLCPP_WARN(this->get_logger(), "Error: %s", e.what());
   }
 }
 
-Pose commander_server::turtlebot4_commander::get_request() {
+commander_server::Pose commander_server::turtlebot4_commander::get_request() {
   boost::system::error_code ec;
 
   // no need to build a string everytime should be const but lazy
-  const std::string request("GET /?agent_id=" + id_ + " HTTP/1.1\r\n\r\n");
-  sock_.send(buffer(request));
+  const std::string request("GET /?agent_id=" + std::to_string(id_) +
+                            " HTTP/1.1\r\n\r\n");
+  socket_.send(boost::asio::buffer(request));
 
   std::string response;
   do {
     char buf[1024];
-    size_t bytes_transferred = sock_.receive(buffer(buf), {}, ec);
+    size_t bytes_transferred =
+        socket_.receive(boost::asio::buffer(buf), {}, ec);
     if (!ec) {
       response.append(buf, buf + bytes_transferred);
     }
   } while (!ec);
 
-  if response
-    .empty() { RCLCPP_WARN(this->get_logger(), "Response was empty"); }
+  if (response.empty()) {
+    RCLCPP_WARN(this->get_logger(), "Response was empty");
+  }
 
   uint64_t split = response.rfind('\n', response.length());
-  string data = response.substr(split, response.length() - split);
+  std::string data = response.substr(split, response.length() - split);
   json json_received = json::parse(data);
 
   // read the position from the json in format [x, y, theta]
   std::vector<json> position = json_received["position"];
-  int x = position[0].get<int>();
-  int y = position[1].get<int>();
+  float x = position[0].get<float>();
+  float y = position[1].get<float>();
   int theta = position[2].get<int>();
 
   return get_pose_stamped(x, y, theta);
@@ -56,32 +63,36 @@ Pose commander_server::turtlebot4_commander::get_request() {
 
 void commander_server::turtlebot4_commander::post_request(Pose pose,
                                                           std::string status) {
-  json::object_t payload;
+  boost::system::error_code ec;
+  json payload = json::object();
   payload["agent_id"] = id_;
   payload["status"] = status;
-  payload["position"] = json::array(
-      {pose.pose.position.x, pose.pose.position.y, pose.pose.orientation.z});
 
   std::string serial_payload = payload.dump();
   std::string request(
-      "POST / HTTP/1.1\r\n" + "Host: " + std::to_string(id_) + " \r\n" +
-      "Connection: close\r\n" + "Accept: */*\r\n" + "User-Agent: " +
-      std::to_string(id_) + "\r\n" + "Content-Type: applications/json\r\n" +
-      "Content-Length: " + std::to_string(serial_payload.length()) + "\r\n" +
-      "\r\n" + serial_payload);
-  sock.send(buffer(request));
+      "POST / HTTP/1.1\r\n Host: \r\n " + std::to_string(id_) + " \r\n" +
+      "Connection: close\r\n Accept: */*\r\n User-Agent: " +
+      std::to_string(id_) +
+      "\r\n Content-Type: applications/json\r\n Content-Length: " +
+      std::to_string(serial_payload.length()) + "\r\n" + "\r\n" +
+      serial_payload);
+  socket_.send(boost::asio::buffer(request));
+
+  std::string response;
   do {
     char buf[1024];
-    size_t bytes_transferred = sock.receive(buffer(buf), {}, ec);
+    size_t bytes_transferred =
+        socket_.receive(boost::asio::buffer(buf), {}, ec);
     if (!ec) {
       response.append(buf, buf + bytes_transferred);
     }
   } while (!ec);
 
-  if response
-    .empty() { RCLCPP_WARN(this->get_logger(), "Response was empty"); }
+  if (response.empty()) {
+    RCLCPP_WARN(this->get_logger(), "Response was empty");
+  }
 
-  RCLCPP_INFO(this->get_logger(), "Response: %s", response);
+  RCLCPP_INFO(this->get_logger(), "Response: %s", response.c_str());
 }
 
 void commander_server::turtlebot4_commander::navigate_to_pose(Pose pose) {
@@ -91,17 +102,21 @@ void commander_server::turtlebot4_commander::navigate_to_pose(Pose pose) {
 }
 
 commander_server::Pose
-commander_server::turtlebot4_commander::get_pose_stamped() {
-  Pose pose = Pose();
+commander_server::turtlebot4_commander::get_pose_stamped(float x, float y,
+                                                         float theta) {
+
+  geometry_msgs::msg::PoseStamped pose;
 
   pose.header.frame_id = "map";
-  pose.header.stamp = self.get_clock().now().to_msg();
+  pose.header.stamp = this->get_clock()->now();
 
   pose.pose.position.x = x;
   pose.pose.position.y = y;
 
-  pose.pose.orientation.z = sin(radians(theta) / 2);
-  pose.pose.orientation.w = cos(radians(theta) / 2);
+  float rads = theta * (PI / 180);
+  pose.pose.orientation.z = sin(rads / 2);
+  pose.pose.orientation.w = cos(rads / 2);
+
   return pose;
 }
 
@@ -111,7 +126,7 @@ void commander_server::turtlebot4_commander::navigate_to_pose_send_goal(
   auto goal_msg = NavigateToPose::Goal();
   goal_msg.pose = pose;
 
-  this->navigate_to_pose_client_ptr_->wait_for_action_server();
+  navigate_to_pose_client_ptr_->wait_for_action_server();
 
   auto send_goal_options =
       rclcpp_action::Client<NavigateToPose>::SendGoalOptions();
@@ -127,8 +142,7 @@ void commander_server::turtlebot4_commander::navigate_to_pose_send_goal(
       &commander_server::turtlebot4_commander::navigate_to_pose_result_callback,
       this, std::placeholders::_1);
 
-  this->navigate_to_pose_client_ptr_->async_send_goal(goal_msg,
-                                                      send_goal_options);
+  navigate_to_pose_client_ptr_->async_send_goal(goal_msg, send_goal_options);
 }
 
 void commander_server::turtlebot4_commander::
@@ -167,15 +181,23 @@ void commander_server::turtlebot4_commander::navigate_to_pose_result_callback(
     RCLCPP_ERROR(this->get_logger(), "Unknown result code");
     return;
   }
-}
 
-int main(int argc, char **argv) {
-  rclcpp::init(argc, argv);
+  void commander_server::turtlebot4_commander::pose_topic_callback(
+      const PoseWithCovariance::SharedPtr msg) {
+    RCLCPP_INFO(this->get_logger(), "Pose: %f, %f, %f",
+                msg->pose.pose.position.x, msg->pose.pose.position.y,
+                msg->pose.pose.orientation.z);
 
-  auto node = std::make_shared<commander_server::turtlebot4_commander>(
-      0, "192.168.0.204", "8080");
+    post_request(msg->pose, "Executing");
+  }
 
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
-}
+  int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+
+    auto node = std::make_shared<commander_server::turtlebot4_commander>(
+        0, "192.168.0.204", 8080);
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+  }
--- src/spl_packages/commander_server/include/commander_server/turtlebot4_commander.hpp
+++ src/spl_packages/commander_server/include/commander_server/turtlebot4_commander.hpp
@@ -1,4 +1,5 @@
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose_with_covariance.hpp"
 #include "irobot_create_msgs/action/dock.hpp"
 #include "irobot_create_msgs/action/undock.hpp"
 #include "nav2_msgs/action/navigate_to_pose.h"
@@ -7,13 +8,18 @@
 #include "rclcpp_action/rclcpp_action.hpp"
 #include <boost/asio.hpp>
 #include <boost/asio/ip/address.hpp>
+#include <cstdint>
+#include <math.h>
 #include <nlohmann/json.hpp>
 #include <string>
 
 namespace commander_server {
 
+#define PI 3.14159265
+
 using json = nlohmann::json;
 using Pose = geometry_msgs::msg::PoseStamped;
+using PoseCovaraince = geometry_msgs::msg::PoseWithCovariance;
 using NavigateToPose = nav2_msgs::action::NavigateToPose;
 using GoalHandleNavigateToPose =
     rclcpp_action::ClientGoalHandle<NavigateToPose>;
@@ -22,23 +28,25 @@ class turtlebot4_commander : public rclcpp::Node {
 
 public:
   turtlebot4_commander(
-      int robot_id, const std::string &ip, const std::string &port,
+      uint32_t id, const std::string &ip, const uint16_t port,
       const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
 
-  void get_request();
+  Pose get_request();
 
   void post_request(Pose pose, std::string status);
 
   void navigate_to_pose(Pose pose);
 
-  Pose get_pose_stamped(float x, float y);
+  Pose get_pose_stamped(float x, float y, float theta);
 
 private:
   const std::string ip_;
-  const std::string port_;
-  int id_;
+  const uint16_t port_;
+  const uint32_t id_;
   rclcpp_action::Client<NavigateToPose>::SharedPtr navigate_to_pose_client_ptr_;
-  ip::tcp::socket sock_;
+  rclcpp::Subscription<PoseWithCovariance>::SharedPtr pose_subscriber_ptr_;
+  boost::asio::io_service io_service_;
+  boost::asio::ip::tcp::socket socket_;
 
   // Follow Waypoints Command
   //====================================================================================
@@ -53,6 +61,8 @@ private:
 
   void navigate_to_pose_result_callback(
       const GoalHandleNavigateToPose::WrappedResult &result);
+
+  void pose_topic_callback(const PoseWithCovariance::SharedPtr msg) const;
 };
 
 } // namespace commander_server
