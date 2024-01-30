e8166fce6bff0f44864da2d80ef321ea901d43d4
jayden <jaydenjfilippi@gmail.com>
2024-01-30 17:07:37 +1100

    fix path

--- src/spl_packages/localization_server/launch/localization.launch.py
+++ src/spl_packages/localization_server/launch/localization.launch.py
@@ -9,74 +9,77 @@ from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
-
     # rviz_config = os.path.join(get_package_share_directory(
     #     'localization_server'), 'config', 'localization_rviz_config.rviz')
-    amcl_yaml = os.path.join(get_package_share_directory(
-        'localization_server'), 'config', 'amcl_config.yaml')
-    map_file = os.path.join(get_package_share_directory(
-        'localization_server'), 'maps', 'full_office.yaml')
+    amcl_yaml = os.path.join(
+        get_package_share_directory("localization_server"), "config", "amcl_config.yaml"
+    )
+    map_file = os.path.join(
+        get_package_share_directory("localization_server"), "maps", "full_office.yaml"
+    )
 
     ARGUMENTS = [
         DeclareLaunchArgument(
-            'namespace',
-            default_value='',
-            description='Robot namespace'),
-
-        DeclareLaunchArgument('use_sim_time', default_value='false',
-                              choices=['true', 'false'],
-                              description='Use sim time')
+            "namespace", default_value="", description="Robot namespace"
+        ),
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="false",
+            choices=["true", "false"],
+            description="Use sim time",
+        ),
     ]
 
-    namespace = LaunchConfiguration('namespace')
-    use_sim_time = LaunchConfiguration('use_sim_time')
+    namespace = LaunchConfiguration("namespace")
+    use_sim_time = LaunchConfiguration("use_sim_time")
 
-    param_substitutions = {
-        'use_sim_time': use_sim_time}
+    param_substitutions = {"use_sim_time": use_sim_time}
 
     configured_params = RewrittenYaml(
         source_file=amcl_yaml,
         root_key=namespace,
         param_rewrites=param_substitutions,
-        convert_types=True)
+        convert_types=True,
+    )
 
     remappings = [
-        ('/tf', 'tf'),
-        ('/tf_static', 'tf_static'),
-        ('/scan', 'scan'),
-        ('/map', 'map')]
+        # ("/tf", "tf"),
+        # ("/tf_static", "tf_static"),
+        # ("/scan", "scan"),
+        # ("/map", "map"),
+    ]
 
     map_server = Node(
-        package='nav2_map_server',
+        package="nav2_map_server",
         namespace=namespace,
-        executable='map_server',
-        name='map_server',
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time},
-                    {'yaml_filename': map_file}]
+        executable="map_server",
+        name="map_server",
+        output="screen",
+        parameters=[{"use_sim_time": use_sim_time}, {"yaml_filename": map_file}],
     )
 
     amcl_server = Node(
-        package='nav2_amcl',
+        package="nav2_amcl",
         namespace=namespace,
-        executable='amcl',
-        name='amcl_server',
-        parameters=[configured_params,
-                    {'tf_options.ignore_time_diff_warning': 1}],
+        executable="amcl",
+        name="amcl_server",
+        parameters=[configured_params, {"tf_options.ignore_time_diff_warning": 1}],
         remappings=remappings,
-        output='screen'
+        output="screen",
     )
 
     lifecycle_manager = Node(
-        package='nav2_lifecycle_manager',
+        package="nav2_lifecycle_manager",
         namespace=namespace,
-        executable='lifecycle_manager',
-        name='lifecycle_manager_localization',
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time},
-                    {'autostart': True},
-                    {'bond_timeout': 0.0},
-                    {'node_names': ['map_server', "amcl_server"]}]
+        executable="lifecycle_manager",
+        name="lifecycle_manager_localization",
+        output="screen",
+        parameters=[
+            {"use_sim_time": use_sim_time},
+            {"autostart": True},
+            {"bond_timeout": 0.0},
+            {"node_names": ["map_server", "amcl_server"]},
+        ],
     )
 
     ld = LaunchDescription(ARGUMENTS)
